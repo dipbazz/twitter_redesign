847841d4da2f7481fe82bb64918f205d5328dcb4
Dipesh Bajgain <dipbazz@gmail.com>
2021-02-23 15:38:27 +0545

    Add create method in opinions controller
    logged in user can create an opinion and some fix with
    UI layout.

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -2,6 +2,29 @@ class OpinionsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @opinion = Opinion.all
+    @opinions = Opinion.all
+    @opinion = Opinion.new
+  end
+
+  def new
+    @opinion = Opinion.new
+  end
+
+  def create
+    @opinion = current_user.opinions.build(opinion_params)
+
+    if @opinion.save
+      flash[:notice] = "Your opinion has been created."
+    else
+      flash[:alert] = "Something went wrong while creating your opinion."
+    end
+
+    redirect_to opinions_path
+  end
+
+  private
+
+  def opinion_params
+    params.require(:opinion).permit(:text)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def alert_class
+    if flash[:notice]
+      return "alert-success"
+    elsif flash[:alert]
+      return "alert-danger"
+    end
+
+    "d-none"
+  end
+
+  def alert_message
+    if flash[:notice]
+      return content_tag :span, flash[:notice]
+    elsif flash[:alert]
+      return content_tag :span, flash[:alert]
+    end
+  end
 end
