b2e994348f1ef94a7db3af586287d616c8ef74eb
Dipesh Bajgain <dipbazz@gmail.com>
2021-02-26 16:47:52 +0545

    Fix dependent issues

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -1,6 +1,6 @@
 class Opinion < ApplicationRecord
   belongs_to :user
-  has_many :likes
+  has_many :likes, dependent: :destroy
 
   validates :text, presence: true, length: { minimum: 5 }
   default_scope { order(created_at: :desc) }
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,12 @@ class User < ApplicationRecord
 
   has_many :opinions
   has_many :followed_users, foreign_key: :follower_id, class_name: 'Following'
-  has_many :follows, through: :followed_users, source: :followed
+  has_many :follows, through: :followed_users, source: :followed, dependent: :destroy
 
   has_many :following_users, foreign_key: :followed_id, class_name: 'Following'
-  has_many :followers, through: :following_users
+  has_many :followers, through: :following_users, dependent: :destroy
 
-  has_many :likes
+  has_many :likes, dependent: :destroy
 
   validates :username, presence: true, uniqueness: true, length: { minimum: 4 }
 
